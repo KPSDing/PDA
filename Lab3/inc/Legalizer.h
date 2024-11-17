c7b8aa000522d89a24e1a85cf391a64d8d39c81b
丁慧慈 <87256542+KPSDing@users.noreply.github.com>
2024-11-17 20:40:31 +0800

    Update Legalizer.h

--- Lab3/inc/Legalizer.h
+++ Lab3/inc/Legalizer.h
@@ -1,5 +1,5 @@
-#ifndef _LEGALIZER_H
-#define _LEGALIZER_H
+#ifndef LEGALIZER_H
+#define LEGALIZER_H
 
 using namespace std;
 
@@ -10,33 +10,50 @@ using namespace std;
 #include <set>
 #include <algorithm>
 #include <fstream>
+#include <sstream>
 #include <unordered_map>
 #include <unordered_set>
+#include <iomanip>
 
+#define PSEUDO -1
 #define FF 0
 #define GATE 1
+#define INSERTFF 2
+#define ILLEGAL_FF 3
 
-struct coor{
-    int x, y;
+struct Point{
+    double x, y;
 };
 
 class Block{
     public:
-        int x, y;
-        int width, height;
-        bool type; // 0 for FFs, 1 for Gates
-        Block(int x, int y, int width, int height, bool type);
+        string name;
+        Point BL;
+        Point original;
+        double width, height;
+        int type; // 0 for FFs, 1 for Gates, 2 for Inserted FFs
     private:    
-
 };
 
-class Placementrow{
-    public:
-        int startX, startY;
-        int siteWidth, siteHeight;
-        int totalNumOfSites;
-    private:
-}
+class CompareByDifference {
+    int target;
+
+public:
+    explicit CompareByDifference(int t) : target(t) {}
+
+    bool operator()(const int& a, const int& b) const {
+        int diff_a = std::abs(a - target);
+        int diff_b = std::abs(b - target);
+
+        if (diff_a == diff_b) {
+            return a < b;
+        }
+
+        return diff_a < diff_b;
+    }
+};
+
+using Map_x_block = map<double, Block*>;
 
 class Legalizer {
     public:
@@ -44,21 +61,40 @@ class Legalizer {
         Legalizer();
         void parser_info(string filename);
         void parser_opt(string filename);
-        void output_file(string filename);
+        // void output_file(string filename);
         void output_draw(string filename);
-        void block_legalize(string blockname);
+        //void block_legalize(string blockname);
+
+        void check_input();
 
         //data member
         unordered_map<string, Block> all_blocks;
-        unordered_map<string, Placementrow> all_rows;
+        vector<Map_x_block> PlacementRow;
 
-        multimap<int, string> x_sorted_blocks;
-        multimap<int, string> y_sorted_blocks;
     private:
-        coor findnearestsite(coor target_xy);
-        coor spiral_search(coor target_xy);
+        bool window_search(Block* tempblock);
+        
+
+        // for block
+        bool check_overlap(Block block);
+        bool check_inside_boundary(Block block);
+        bool check_legal(Block block);
+        void Make_onsite(Block* block);
+        int Get_PLR_index(Block block);
+
 
         double alpha, beta;
-        int die_x, die_y, die_width, die_height;
+        double die_x, die_y, die_width, die_height;
         double cost;
+        double max_width, max_height;
+
+        // for placementrow
+        double Placementrow_startX, Placementrow_startY, Placementrow_siteWidth, Placementrow_siteHeight, Placementrow_totalNumOfSites, Placementrow_rightmost;
+        void insert_block(Block* new_block);
+        void remove_block(string blockname);
+        
+        // for test legal
+        double total_success;
 };
+
+#endif
